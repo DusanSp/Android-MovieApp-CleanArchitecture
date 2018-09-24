61fd87ec2890fdd13a8f167b48b0b737f1833203
Dusan Spasic <dusanspasic29@gmail.com>
2018-09-24 21:15:19 +0200

    Refactored versions

--- app/src/main/java/com/example/dusan/movieapp/presentation/view/application/MovieApplication.java
+++ app/src/main/java/com/example/dusan/movieapp/presentation/view/application/MovieApplication.java
@@ -10,41 +10,41 @@ import com.google.gson.GsonBuilder;
 
 public class MovieApplication extends Application {
 
-    private static MovieApplication sInstance;
-    private static Gson sGson;
+  private static MovieApplication sInstance;
+  private static Gson sGson;
 
-    public static Application getApplication() {
-        return sInstance;
+  public static Application getApplication() {
+    return sInstance;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    sInstance = this;
+
+    iniGson();
+  }
+
+  public static Gson getGson() {
+    return sGson;
+  }
+
+  private void iniGson() {
+    sGson = new GsonBuilder()
+        .serializeNulls()
+        .create();
+  }
+
+  public static boolean haveInternetConnection() {
+    ConnectivityManager cm =
+        (ConnectivityManager) getApplication().getApplicationContext()
+            .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+    if (cm == null) {
+      return false;
     }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        sInstance = this;
-
-        iniGson();
-    }
-
-    public static Gson getGson() {
-        return sGson;
-    }
-
-    private void iniGson() {
-        sGson = new GsonBuilder()
-                .serializeNulls()
-                .create();
-    }
-
-    public static boolean haveInternetConnection() {
-        ConnectivityManager cm =
-                (ConnectivityManager) getApplication().getApplicationContext()
-                        .getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        if (cm == null) {
-            return false;
-        }
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
-    }
+    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+    return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+  }
 }
